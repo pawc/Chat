c76dfb2aea7e5f6edcbf05ca329e4e2d866ae194
pawc <pswid89@gmail.com>
2015-08-10 17:27:32 +0200

    added some tests

---
+++ ChatServer/src/test/java/pawc/chat/shared/model/DataTest.java
@@ -0,0 +1,42 @@
+package pawc.chat.shared.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mockito.Mockito;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import pawc.chat.shared.model.Data;
+
+
+public class DataTest 
+    extends TestCase
+{
+    public DataTest( String testName )
+    {
+        super( testName );
+    }
+
+    public static Test suite()
+    {
+        return new TestSuite( DataTest.class );
+    }
+    
+    public void testData()
+    {
+        List list = new ArrayList<String>();
+        list.add("first nick");
+        list.add("second nick");
+        Data data = new Data("nicks", list);
+        assertTrue(data instanceof Data);
+        assertTrue(data.getCommand().equals("nicks"));
+        assertTrue(data.getArguments() instanceof List);
+        List readList = (List) data.getArguments();
+        assertTrue(readList.get(0).equals("first nick"));
+        assertTrue(readList.get(1).equals("second nick"));
+        
+    }
+}
---
+++ ChatServer/src/test/java/pawc/chat/shared/model/PrivateMessageTest.java
@@ -0,0 +1,25 @@
+package pawc.chat.shared.model;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class PrivateMessageTest extends TestCase{
+
+    public PrivateMessageTest(String name){
+        super(name);
+    }
+    
+    public static Test suite(){
+        return new TestSuite(PrivateMessageTest.class);
+    }
+    
+    public void TestPrivateMessage(){
+        PrivateMessage message = new PrivateMessage("john", "paul", "hello");
+        assertTrue(message instanceof PrivateMessage);
+        assertTrue(message.getSender().equals("john"));
+        assertTrue(message.getRecipient().equals("paul"));
+        assertTrue(message.getMessage().equals("hello"));
+    }
+    
+}
